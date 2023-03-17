137d846038225a3dfef91afbdf48c67f87ddecac
Yazdun <yazduntube@gmail.com>
2023-03-17 20:56:43 +0330

    commit

--- src/components/pagination/index.jsx
+++ src/components/pagination/index.jsx
@@ -1,10 +1,14 @@
 import { Container } from '../container'
 import css from './styles.module.css'
 import { IoCaretBack, IoCaretForward } from 'react-icons/io5'
+import { useScrollDirection } from '../../hooks'
+import cn from 'classnames'
 
 export const Pagination = () => {
+  const scrollDirection = useScrollDirection()
+
   return (
-    <div className={css.wrapper}>
+    <div className={cn(css.wrapper, scrollDirection > 100 && css.scrolled)}>
       <Container className={css.container}>
         <span className={css.title}>Users List</span>
         <ul>
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,2 +1,3 @@
 export { useLocalStorage } from './useLocalStorage'
 export { useOnClickOutside } from './useOnClickoutside'
+export { useScrollDirection } from './useScrollDirection'
